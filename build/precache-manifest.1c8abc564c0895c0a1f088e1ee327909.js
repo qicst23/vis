63e1e0d3638d7bd507faf435666c76a6977807c7
qicst2345 <qicst2345@gmail.com>
2025-12-01 22:36:15 -0600

    2 new tab in the visual

--- build/precache-manifest.1c8abc564c0895c0a1f088e1ee327909.js
+++ build/precache-manifest.1c8abc564c0895c0a1f088e1ee327909.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "db56ce0748ed485766b1f907eca0ce70",
+    "revision": "a495bf2f3dc56c5d4f5ab0d86912eb12",
     "url": "/index.html"
   },
   {
-    "revision": "14a122b7c5dd8054e4f3",
+    "revision": "390bd368ca45ee823008",
     "url": "/static/css/2.d7cfef25.chunk.css"
   },
   {
-    "revision": "3ffee7eb215401eb0d05",
+    "revision": "1d9fcd45aef16863b36b",
     "url": "/static/css/main.9beeabf9.chunk.css"
   },
   {
-    "revision": "14a122b7c5dd8054e4f3",
-    "url": "/static/js/2.993c61f7.chunk.js"
+    "revision": "390bd368ca45ee823008",
+    "url": "/static/js/2.d17ce8cd.chunk.js"
   },
   {
     "revision": "774152ec3dfb5dadc6880f2e7f79e400",
-    "url": "/static/js/2.993c61f7.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.d17ce8cd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3ffee7eb215401eb0d05",
-    "url": "/static/js/main.e36eb74c.chunk.js"
+    "revision": "1d9fcd45aef16863b36b",
+    "url": "/static/js/main.f592dd2b.chunk.js"
   },
   {
     "revision": "40ba6fc54daca019862a",
